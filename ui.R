bb3037ea4bbc4f0315fc481f00181a8265b25fd5
hmartysci <75278768+hmartysci@users.noreply.github.com>
2022-05-24 00:46:34 -0400

    Move helper functions from files in the R folder to server.R and ui.R

--- ui.R
+++ ui.R
@@ -1,5 +1,37 @@
 library(shiny)
 
+#' Creates a new tab for a navlistPanel.
+#'
+#' Creates a new tab displaying a bar graph for a dataset. The tab has a
+#' selectInput for selecting one of the attributes in the attributes vector
+#' parameter. The selected attribute is the categorical variable according to
+#' which data from the dataset is categorized to plot a bar graph.
+#'
+#' @param tab_name The name of the new tab.
+#' @param select_name The name of the selectInput.
+#' @param attributes A vector of attribute strings for the selectInput.
+#' @param plot_name The name of the bar graph.
+#'
+#' @return A new tabPanel.
+create_tab <- function(tab_name, select_name, attributes, plot_name) {
+  tabPanel(
+    tab_name,
+    fluidRow(
+      column(
+        4, offset = 4,
+        # a grey well containing the select list of attributes
+        wellPanel(
+          selectInput(select_name, "Attribute:", attributes)
+        )
+      ),
+    ),
+    fluidRow(
+      style = "margin-bottom:20px;",
+      column(10, offset = 1, plotOutput(plot_name))
+    )
+  )
+}
+
 fluidPage(
   tags$head(includeHTML("google-analytics.js")),
   style = "margin: 10px; font-family: \"Times New Roman\";
