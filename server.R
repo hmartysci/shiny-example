5a36da92cc8e4864f039ba8c1029ea82611e279e
hmartysci <75278768+hmartysci@users.noreply.github.com>
2022-05-23 22:12:53 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,38 +1,4 @@
 library(shiny)
-library(tidyverse)
-
-# Creates a bar chart categorizing the data in the input data.frame data
-# according to values of the categorical variable named attribute, where the
-# height of each bar represents the amount of data in each category. The input
-# entity is the name of what the data in the dataset represents (for example,
-# "Students" for HairEyeColor).
-create_geom_bar_chart <- function(attribute, data, entity) {
-  # Use of get(): https://cran.r-project.org/doc/FAQ/R-FAQ.html, section 7.21
-  # "How can I turn a string into a variable?"
-  ggplot(data, aes(get(attribute))) +
-    geom_bar(fill = "darkolivegreen2") +
-    labs(
-      x = attribute,
-      y = "Frequency",
-      title = paste(entity, "by", attribute)
-    )
-}
-
-# Creates a bar chart categorizing the data in the input data.frame data
-# according to values of the categorical variable named attribute, where the
-# height of each bar is a value in data. The input entity is the name of what
-# the data in the dataset represents (for example, "Students" for HairEyeColor).
-create_geom_col_chart <- function(attribute, data, entity) {
-  # Use of get(): https://cran.r-project.org/doc/FAQ/R-FAQ.html, section 7.21
-  # "How can I turn a string into a variable?"
-  ggplot(data, aes(get(attribute), Freq)) +
-    geom_col(fill = "darkolivegreen2") +
-    labs(
-      x = attribute,
-      y = "Frequency",
-      title = paste(entity, "by", attribute)
-    )
-}
 
 server <- function(input, output) {
   # Renders the bar graph for the first tab using the data in HairEyeColor
@@ -45,8 +11,8 @@ server <- function(input, output) {
   # state.division, and state.region (among the state datasets about "US State
   # Facts and Figures")
   data2 <- data.frame(Abbreviation = state.abb,
-                     Division = state.division,
-                     Region = state.region)
+                      Division = state.division,
+                      Region = state.region)
   output$plot2 <- renderPlot({
     create_geom_bar_chart(input$select2, data2, "States")
   })
